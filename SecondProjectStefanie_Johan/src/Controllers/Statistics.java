81289e6695ba4b61732228eee0481acb74e75439
stefaniehd <sahurtadod@est.utn.ac.cr>
2017-08-13 13:47:36 -0600

    Statistics
    Se crea la lógica para generar el reporte de estadisticas

---
+++ SecondProjectStefanie_Johan/src/Controllers/Statistics.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controllers;
+
+/**
+ *
+ * @author Magdalena
+ */
+public class Statistics {
+    private Models.Statistics statistics;
+
+    public Statistics() {
+        statistics = new Models.Statistics();
+    }
+    
+    
+}
---
+++ SecondProjectStefanie_Johan/src/Models/Statistics.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+import Utils.FileManager;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author Magdalena
+ */
+public class Statistics {
+
+    private String loser;
+    private String winner;
+    private int empates;
+    private int races;
+    private LinkedList<Models.Swimmer> swimmer;
+    private FileManager fileManager;
+
+    public Statistics() {
+        fileManager = new FileManager();
+    }
+
+    public String getLoser() {
+        return loser;
+    }
+
+    public void setLoser(String loser) {
+        this.loser = loser;
+    }
+
+    public String getWinner() {
+        return winner;
+    }
+
+    public void setWinner(String winner) {
+        this.winner = winner;
+    }
+
+    public int getEmpates() {
+        return empates;
+    }
+
+    public void setEmpates(int empates) {
+        this.empates = empates;
+    }
+
+    public int getRaces() {
+        return races;
+    }
+
+    public void setRaces(int races) {
+        this.races = races;
+    }
+
+    public LinkedList<Swimmer> getSwimmer() {
+        return swimmer;
+    }
+
+    public void setSwimmer(LinkedList<Swimmer> swimmer) {
+        this.swimmer = swimmer;
+    }
+    
+    private void select(){
+        
+    }
+
+    public String generateReport() {
+        String result = "";
+        for (int i = 0; i < swimmer.size(); i++) {
+            result += swimmer.get(i).getName() + " " + swimmer.get(i).getLastName() + " ha ganado "
+                    + swimmer.get(i).getGanadas() + " veces";
+        }
+        result += "Carreras efectuadas: " + this.getRaces();
+        result += ganador() + " ha ganado más veces.";
+        result += perdedor()+ " ha perdido más veces.";
+        result += "Empates registrados: " + this.getEmpates();
+        fileManager.write("statistics.txt", this.getRaces()+";"+this.getEmpates());
+        return result;
+    }
+
+    private String ganador() {
+        String nombre = "";
+        try {
+            nombre = swimmer.get(0).getName() + " " + swimmer.get(0).getLastName();
+            for (int i = 1; i < swimmer.size(); i++) {
+                if (swimmer.get(0).getGanadas() < swimmer.get(i).getGanadas()) {
+                    nombre = swimmer.get(i).getName() + " " + swimmer.get(i).getLastName();
+                }
+            }
+        } catch (Exception e) {
+        }
+        return nombre;
+    }
+    
+    private String perdedor() {
+        String nombre = "";
+        try {
+            nombre = swimmer.get(0).getName() + " " + swimmer.get(0).getLastName();
+            for (int i = 1; i < swimmer.size(); i++) {
+                if (swimmer.get(0).getPerdidas()< swimmer.get(i).getPerdidas()) {
+                    nombre = swimmer.get(i).getName() + " " + swimmer.get(i).getLastName();
+                }
+            }
+        } catch (Exception e) {
+        }
+        return nombre;
+    }
+}
